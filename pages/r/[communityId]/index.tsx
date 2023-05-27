bc46e393e52ed0e26cc811e1f21110e50b29849a
artreimus <63195930+artreimus@users.noreply.github.com>
2023-05-27 10:58:12 +0800

    convert to client side routing

--- pages/r/[communityId]/index.tsx
+++ pages/r/[communityId]/index.tsx
@@ -5,70 +5,62 @@ import Header from '@/components/Community/Header';
 import PageContent from '@/components/Layout/PageContent';
 import Posts from '@/components/Posts';
 import { firestore } from '@/firebase/clientApp';
-import {
-  Community,
-  selectCommunitiesState,
-  setCurrentCommunity,
-} from '@/store/communitiesSlice';
-import { error } from 'console';
+import { Community, setCurrentCommunity } from '@/store/communitiesSlice';
 import { doc, getDoc } from 'firebase/firestore';
-import { GetServerSidePropsContext } from 'next';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import safeJsonStringify from 'safe-json-stringify';
 
 type CommunityPageProps = {
   communityData: Community;
 };
 
-// const testFunction = async () => {
-//   const context = { query: { communityId: '' } };
-//   try {
-//     const communityDocRef = doc(
-//       firestore,
-//       'communities',
-//       context.query.communityId as string
-//     );
-
-//     const communityDoc = await getDoc(communityDocRef);
-
-//     return {
-//       props: {
-//         communityData: communityDoc.exists()
-//           ? JSON.parse(
-//               safeJsonStringify({
-//                 id: communityDoc.id,
-//                 ...communityDoc.data(),
-//               })
-//             )
-//           : '',
-//       },
-//     };
-//   } catch (error) {
-//     console.error('getServerSideProps error', error);
-//   }
-// };
-
-const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+const CommunityPage: React.FC<CommunityPageProps> = () => {
+  const router = useRouter();
+  const { communityId } = router.query;
+  const [communityData, setCommunityData] = useState<Community>();
   const dispatch = useDispatch();
 
-  const communityId2 = 'hello world';
-  console.log('  communityId2.length', communityId2.length);
-  const communityId = ['hello world'];
-  console.log('  communityId.length', communityId.length);
-
-  const { currentCommunity } = useSelector(selectCommunitiesState);
-
   useEffect(() => {
+    const fetchCommunityData = async (communityId: string) => {
+      const context = { query: { communityId: communityId } };
+      try {
+        const communityDocRef = doc(
+          firestore,
+          'communities',
+          context.query.communityId as string
+        );
+
+        const communityDoc = await getDoc(communityDocRef);
+
+        setCommunityData(
+          communityDoc.exists()
+            ? JSON.parse(
+                safeJsonStringify({
+                  id: communityDoc.id,
+                  ...communityDoc.data(),
+                })
+              )
+            : ''
+        );
+      } catch (error) {
+        console.error('getServerSideProps error', error);
+      }
+    };
+
+    if (communityId) {
+      fetchCommunityData(communityId as string);
+    }
+
     if (communityData) {
       dispatch(setCurrentCommunity(communityData));
     }
-  }, [communityData, dispatch]);
+  }, [dispatch, communityId]);
 
   if (!communityData) {
     return <CommunityNotFound />;
   }
-  // console.log(communityData);
 
   return (
     <>
@@ -86,35 +78,4 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   );
 };
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const communityId = context.query.communityId
-    ? context.query.communityId.length === 1
-      ? context.query.communityId[0]
-      : context.query.communityId
-    : '';
-
-  try {
-    const communityDocRef = doc(firestore, 'communities', 'ArtReimus');
-
-    const communityDoc = await getDoc(communityDocRef);
-
-    if (!communityDoc.exists()) return { props: { communityData: '' } };
-
-    return {
-      props: {
-        context: { ...context.query },
-        communityData: communityDoc.exists()
-          ? JSON.parse(
-              safeJsonStringify({
-                id: communityDoc.id,
-                ...communityDoc.data(),
-              })
-            )
-          : '',
-      },
-    };
-  } catch (error) {
-    console.error('getServerSideProps error', error);
-  }
-}
 export default CommunityPage;
